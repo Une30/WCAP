29499568e5f5474b321789f8594209f36026fa5c
littleali <ali.nosrati72@gmail.com>
2015-05-22 11:55:04 +0430

    change javascript link

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( jquery-1.9.1.min.js )
+Rails.application.config.assets.precompile += %w( jquery.jcanvas.min.js )
+Rails.application.config.assets.precompile += %w( jquery.bart.js )
